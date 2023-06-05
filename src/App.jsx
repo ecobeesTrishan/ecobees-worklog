d998b27f0737ac3c509a9b2cb8bcc1a0a98ae220
Trishan Wagle <trishanwagle@pop-os.localdomain>
2023-06-05 09:39:33 +0545

    init: initialized project.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app flex justify-center items-center mt-4">
+      <h1 className="text-2xl">EcoBees Work Log Management System</h1>
+    </div>
+  )
+}
+
+export default App;
