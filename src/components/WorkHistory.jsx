2f6badede1877372b291023e4faaade99688e15d
Trishan Wagle <trishan.ecobees@gmail.com>
2023-06-20 11:33:13 +0545

    fix: formatted started date at work history.

--- src/components/WorkHistory.jsx
+++ src/components/WorkHistory.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from "react"
 import { getDocs, query, where } from "firebase/firestore"
 import Select from 'react-select'
+import moment from "moment"
 import { colRef, projectsColRef } from "src/firebase"
 import { AuthContext } from "contexts"
 
@@ -126,35 +127,40 @@ const WorkHistory = () => {
                                 <tbody className="bg-white divide-y divide-gray-200">
                                     {tasks.length > 0
                                         ?
-                                        tasks.map((task) => (
-                                            <tr key={task.id}>
-                                                <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6">
-                                                    {task.startedAt}
-                                                </td>
+                                        tasks.map((task) => {
+                                            const { startedAt } = task
+                                            const date = moment(startedAt).format("LL")
 
-                                                <td className="px-3 py-4 text-sm text-gray-900 whitespace-nowrap">
-                                                    {task.project}
-                                                </td>
+                                            return (
+                                                <tr key={task.id}>
+                                                    <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6">
+                                                        {date}
+                                                    </td>
 
-                                                <td className="px-3 py-4 text-sm text-gray-900 whitespace-nowrap">
-                                                    {task.ticket}
-                                                </td>
+                                                    <td className="px-3 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                                        {task.project}
+                                                    </td>
 
-                                                <td className="px-3 py-4 text-sm text-gray-900 whitespace-nowrap">
-                                                    {task.hoursBilled}
-                                                </td>
+                                                    <td className="px-3 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                                        {task.ticket}
+                                                    </td>
 
-                                                <td className="px-3 py-4 text-sm text-gray-900 capitalize whitespace-nowrap">
-                                                    {task.type}
-                                                </td>
+                                                    <td className="px-3 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                                        {task.hoursBilled}
+                                                    </td>
 
-                                                <td className="px-3 py-4 text-sm text-indigo-600 cursor-pointer whitespace-nowrap hover:text-indigo-900 hover:underline">
-                                                    <a href={task.prLink} target="_blank">
-                                                        {task.prLink}
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        ))
+                                                    <td className="px-3 py-4 text-sm text-gray-900 capitalize whitespace-nowrap">
+                                                        {task.type}
+                                                    </td>
+
+                                                    <td className="px-3 py-4 text-sm text-indigo-600 cursor-pointer whitespace-nowrap hover:text-indigo-900 hover:underline">
+                                                        <a href={task.prLink} target="_blank">
+                                                            {task.prLink}
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
                                         :
                                         <tr>
                                             <td align="center" className="p-6 opacity-70" colSpan={"6"}>
