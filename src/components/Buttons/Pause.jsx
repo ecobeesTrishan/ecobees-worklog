228fd9b0aa4f0e27693e0a5c659d31b4e3cb2b7a
Trishan Wagle <trishanwagle@pop-os.localdomain>
2023-06-21 13:51:02 +0545

    feat: added icons from heroicons/react.

--- src/components/Buttons/Pause.jsx
+++ src/components/Buttons/Pause.jsx
@@ -1,3 +1,5 @@
+import { PauseIcon } from "@heroicons/react/24/outline"
+
 const Pause = ({ onClick }) => {
     return (
         <div className="flex flex-col items-center justify-center gap-2 mt-10">
@@ -6,19 +8,9 @@ const Pause = ({ onClick }) => {
                 id="user-action-pause"
                 onClick={onClick}
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
+                <PauseIcon
                     className="w-20 p-4 transition ease-in-out rounded-md bg-slate-300 hover:bg-slate-200"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 5.25v13.5m-7.5-13.5v13.5"
-                    />
-                </svg>
+                />
             </button>
 
             <p className="opacity-70">
--- src/components/Common/CloseModal.jsx
+++ src/components/Common/CloseModal.jsx
@@ -1,3 +1,5 @@
+import { XCircleIcon } from "@heroicons/react/24/outline"
+
 const CloseModal = ({ id, onClick }) => {
     return (
         <button
@@ -5,20 +7,9 @@ const CloseModal = ({ id, onClick }) => {
             id={id}
             onClick={onClick}
         >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
+            <XCircleIcon
                 className="absolute right-0 mr-8 cursor-pointer w-7"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-            </svg>
+            />
         </button>
     )
 }
