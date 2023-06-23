13be12dadefb75e161e37f0498f9ea132f6ca9b2
Trishan Wagle <trishanwagle@pop-os.localdomain>
2023-06-23 13:58:38 +0545

    fix: ready for production

--- vite.config.js
+++ vite.config.js
@@ -14,35 +14,33 @@ export default defineConfig({
         "description": "EcoBees Work Log Management System",
         "icons": [
           {
-            "src": "/manifest-icon-192.maskable.png",
+            "src": "/manifest-icon-192_maskable.png",
             "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
+            "type": "image/png"
           },
           {
-            "src": "/manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-          },
-          {
-            "src": "/manifest-icon-512.maskable.png",
+            "src": "/manifest-icon-512_maskable.png",
             "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
+            "type": "image/png"
           },
           {
-            "src": "/manifest-icon-512.maskable.png",
-            "sizes": "512x512",
+            "src": "/apple.png",
+            "sizes": "180x180",
             "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "apple touch icon"
+          },
+          {
+            "src": "/maskable_iconx225.png",
+            "sizes": "225x225",
+            "type": "image/png",
+            "purpose": "any maskable"
           }
         ],
         "theme_color": "#fdb517",
         "background_color": "#ffffff",
         "display": "standalone",
-        "scope": "/",
-        "start_url": "/",
+        "scope": "https://ecobees.vercel.app/",
+        "start_url": "https://ecobees.vercel.app/signin",
         "orientation": "portrait-primary"
       }
     })
