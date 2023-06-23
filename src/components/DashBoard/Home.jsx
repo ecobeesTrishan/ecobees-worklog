25a74ec91d160dacbd752dc2610ae44799607cce
Trishan Wagle <trishanwagle@pop-os.localdomain>
2023-06-23 13:59:30 +0545

    fix: removed un-necessary console logs.

--- src/components/DashBoard/Home.jsx
+++ src/components/DashBoard/Home.jsx
@@ -1,24 +1,10 @@
 import { useContext } from "react";
-import { query, where, onSnapshot } from "firebase/firestore";
 import { AuthContext } from "contexts";
-import { colRef } from "src/firebase";
 import StopWatch from "./StopWatch";
 
 const Home = () => {
     const userContext = useContext(AuthContext);
     const { user } = userContext;
-
-    const firebaseQuery = user?.displayName && query(colRef, where("user.id", "==", user.uid), where("status", "!=", "completed"));
-    user?.displayName && onSnapshot(firebaseQuery, (snapshot) => {
-        const allDocs = snapshot.docs;
-        const tasks = [];
-        allDocs.map((doc) => {
-            tasks.push({
-                ...doc.data(), id: doc.id
-            });
-        });
-        console.log(tasks);
-    });
 
     return (
         <div className="relative flex flex-col items-center font-primary">
